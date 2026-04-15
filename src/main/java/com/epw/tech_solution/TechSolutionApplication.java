de4d0e45dc6e3fde347da3ca5cb86b0c0964d571
santiagourbano1 <jeiver.3762012428@ucaldas.edu.co>
2026-04-15 07:41:21 -0500

    Initial Spring Boot project setup with PostgreSQL

---
+++ src/main/java/com/epw/tech_solution/TechSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epw/tech_solution/TechSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
